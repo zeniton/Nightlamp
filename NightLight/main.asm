ea1bcce2cd47ce5c192ab3e0d5dbbc57c5cd4ed6
Stoffel van Aswegen <stoffel@zeniton.co.za>
2019-10-07 21:33:43 +0200

    Add Motion Detector ISR & code

--- NightLight/main.asm
+++ NightLight/main.asm
@@ -6,41 +6,41 @@
 ; clock frequency: 1 MHz
 ; Function: Night light controller with motion detection
 ;
-;                                         +5V
-;                          ATTiny85        |
-;                         +---------+      |
-;                         | 1     8 |------+
-;                         |         |
-;                         | 2     7 |
+;                +5V       ATTiny85        +5V
+;                 |       +---------+       |
+;                 +--1k---| 1     8 |-------+
+;                   Reset |         |
+;                         | 2     7 |<-PB2/INT0--------> Motion detector
 ;                         |         |
 ;                         | 3     6 |
 ;                         |         |
-;                 +-------| 4     5 |------+----> Lamp
-;                 |       +---------+      |
-;                 |                       _|_
-;                 |                      _\_/_ LED
-;                 |                        |
-;                 |                        >
-;                 |                        < 220R
-;                _|_                      _|_
-;                ///                      ///
+;                 +-------| 4     5 |>-PB0--+----------> Lamp
+;                 |       +---------+      _|_
+;                 |                       _\_/_ LED
+;                 |                         |
+;                 |                        220R
+;                _|_                       _|_
+;                ///                       ///
 										     
 .nolist
 .include "tn85def.inc"
 .list
 
-.def status		= r15	;Copy of Status Register
-.def tmp		= r16	;General accumulator
-.def tcint		= r17	;Timer Compare Interrupt counter
+.def status	= r15	;Copy of Status Register
+.def tmp	= r16	;General register
+.def tcint	= r17	;Timer Compare Interrupt counter
 .def seconds	= r18	;Seconds counter
 .def minutes	= r19	;Minutes counter
-.def hours		= r20	;Hours counter
+.def hours	= r20	;Hours counter
+.def system	= r21	;System flags: bit0=1/0(movement/not)
 
-.equ LAMP		= PB0
+.equ LAMP	= PB0	;Lamp (output)
+.equ MOTION	= PB2	;Motion detector (input)
+.equ MOVEMENT	= 0	;System flag
 
 .org 0x0000
 rjmp START				;0x0000 RESET
-reti					;0x0001 INT0
+rjmp ISR_1				;0x0001 INT0
 reti					;0x0002 PCINT0
 reti					;0x0003 TIMER1_COMPA
 reti					;0x0004 TIMER1_OVF
@@ -56,14 +56,18 @@ reti					;0x000D USI_START
 reti					;0x000E USI_OVF
 
 
-ISR_A:					;Timer Output Compare
-	in status,SREG		;Save status
-	dec tcint
-	brne EXIT_ISR_A
+ISR_1:	in	status,SREG		;Save status
+	ori	system,(1<<MOVEMENT)	;Set system flag
+	out	SREG,status		;Restore status
+	reti
+
+
+ISR_A:	in status,SREG			;Save status
+	dec tcint			;Interrupt counter
+	brne EXIT_A
 	ldi tcint,4			;Reset counter
 	dec seconds
-EXIT_ISR_A:
-	out SREG,status		;Restore status
+EXIT_A:	out SREG,status			;Restore status
 	reti
 
 
@@ -71,43 +75,69 @@ EXIT_ISR_A:
 Sub_TimerOn:
 	ldi tcint,4			;Reset interrupt counter
 	ldi tmp,(1<<CS02)|(1<<CS00)
-	out TCCR0B,tmp		;Prescale Clock/1024
+	out TCCR0B,tmp			;Prescale Clock/1024
 	ret
 
 Sub_TimerOff:
 	clr tmp
-	out TCCR0B,tmp		;Stop timer
+	out TCCR0B,tmp			;Stop timer
 	ret
+;=================
 
 
 START:
-	;PORTB setup
-	ldi tmp,(1<<LAMP)	;Lamp output
-	out DDRB,tmp
-	com tmp				;1's compliment to flip bits
-	out PORTB,tmp		;Enable input pull-ups
-	
 	;Timer0 setup
 	;	With the 1MHz clock prescaled by 1024, the effective rate is 976kHz
 	;	976 = 244 * 4
 	;	Setting the Output Compare Register to 244, the TIMER0_COMPA interrupt will fire every 250ms
 	;	Count 4 interrupts to measure 1s
-	ldi tmp,(1<<WGM01)	;CTC mode
+	ldi tmp,(1<<WGM01)		;CTC mode
 	out TCCR0A,tmp
 	ldi tmp,244			;TIMER0 compare value
 	out OCR0A,tmp
 	ldi tmp,(1<<OCIE0A)
-	out TIMSK,tmp		;Enable output compare interrupt
+	out TIMSK,tmp			;Enable output compare interrupt
 
-	;Setup timing counters
-	ldi seconds,1
-	rcall Sub_TimerOn
+	;Setup PORTB
+	ldi	tmp,(1<<DDB0)
+	out	DDRB,tmp		;Lamp output
+	ldi	tmp,(1<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1)
+	out	PORTB,tmp		;Enable input pull-ups
 
-	sei					;Enable global interrupts
+	;Setup INT0 external interrupt
+	clr	tmp 			;Trigger INT0 interrupt on low level
+	out	MCUCR,tmp
+	ldi	tmp,(1<<INT0)
+	out	GIMSK,tmp		;Enable INT0 interrupt
+
+	cbi	PORTB,LAMP		;Lamp off
+	clr	system
+	sei				;Enable global interrupts
 
 LOOP:
-	tst seconds			;=0?
-	brne LOOP
-	sbi PINB,LAMP		;Toggle LAMP
-	ldi seconds,1		;Restart the counter
-	rjmp LOOP
+	sbrs	system,MOVEMENT
+	rjmp	LOOP
+	sbi	PORTB,LAMP		;Lamp on
+;	ldi	seconds,3
+	ldi	minutes,1
+	rcall	Sub_TimerOn
+	TMR_LOOP:
+		tst	seconds
+		brne	TMR_LOOP
+		tst	minutes
+		breq	TST_HOURS
+		dec	minutes
+		ldi	seconds,60
+		rjmp	TMR_LOOP
+		TST_HOURS:
+			tst	hours
+			breq	EXIT_TMR_LOOP
+			dec	hours
+			ldi	minutes,59
+			ldi	seconds,60
+			rjmp	TMR_LOOP
+	EXIT_TMR_LOOP:
+	andi	system,(0<<MOVEMENT)	;Clear system flag
+	cbi	PORTB,LAMP		;Lamp off
+	rcall	Sub_TimerOff
+	rjmp	LOOP
